0b71aaba7c530ab48f70047115334705fd72e134
STEFANINI-DOM\jhsantos3 <jhsantos3@stefanini.com>
2022-11-11 17:29:49 -0300

    create room

--- service/src/test/java/br/com/sw2you/realmeet/unit/RoomServiceUnitTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/unit/RoomServiceUnitTest.java
@@ -2,15 +2,19 @@ package br.com.sw2you.realmeet.unit;
 
 import static br.com.sw2you.realmeet.utils.MapperUtils.roomMapper;
 import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
+import static br.com.sw2you.realmeet.utils.TestDataCreator.newCreateRoomDTO;
+import static br.com.sw2you.realmeet.utils.TestDataCreator.newRoomBuilder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import br.com.sw2you.realmeet.core.BaseUnitTest;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
 import br.com.sw2you.realmeet.exception.RoomNotFoundException;
 import br.com.sw2you.realmeet.service.RoomService;
-import br.com.sw2you.realmeet.utils.TestDataCreator;
+import br.com.sw2you.realmeet.validator.RoomValidator;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,16 +24,19 @@ class RoomServiceUnitTest extends BaseUnitTest {
     private RoomService victim;
 
     @Mock
+    private RoomValidator roomValidator;
+
+    @Mock
     private RoomRepository roomRepository;
 
     @BeforeEach
     void setupEach() {
-        victim = new RoomService(roomRepository, roomMapper());
+        victim = new RoomService(roomRepository, roomValidator, roomMapper());
     }
 
     @Test
     void testGetRoomSucess() {
-        var room = TestDataCreator.newRoomBuilder().id(DEFAULT_ROOM_ID).build();
+        var room = newRoomBuilder().id(DEFAULT_ROOM_ID).build();
 
         when(roomRepository.findByIdAndActive(DEFAULT_ROOM_ID, true)).thenReturn(Optional.of(room));
         var dto = victim.getRoom(DEFAULT_ROOM_ID);
@@ -42,5 +49,16 @@ class RoomServiceUnitTest extends BaseUnitTest {
     void testGetRoomNotFound() {
         when(roomRepository.findByIdAndActive(DEFAULT_ROOM_ID, true)).thenReturn(Optional.empty());
         assertThrows(RoomNotFoundException.class, () -> victim.getRoom(DEFAULT_ROOM_ID));
+    }
+
+    @Test
+    void testCreateRoomSuccess() {
+        var createRoomDTO = newCreateRoomDTO();
+        var roomDTO = victim.createRoom(createRoomDTO);
+
+        assertEquals(createRoomDTO.getName(), roomDTO.getName());
+        assertEquals(createRoomDTO.getSeats(), roomDTO.getSeats());
+
+        verify(roomRepository).save(any());
     }
 }
